44181c654e7ebcf5883d510c4417b63d624ba761
Matt Breckon <matt@mattbreckon.com>
2018-12-02 00:51:52 -0500

    move freestyle to /

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -45,6 +45,8 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/vanilla-ui';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
