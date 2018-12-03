00029e9c9a7188ba2b021d2ee6424d2f88344616
Matt Breckon <matt@mattbreckon.com>
2018-12-03 10:11:30 -0500

    update naming convention docs

---
+++ tests/dummy/app/components/docs/naming-conventions/helper.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../../templates/components/docs/naming-conventions/helper';
+
+export default Component.extend({
+  layout
+});
---
+++ tests/dummy/app/components/docs/naming-conventions/modifier.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../../templates/components/docs/naming-conventions/modifier';
+
+export default Component.extend({
+  layout
+});
