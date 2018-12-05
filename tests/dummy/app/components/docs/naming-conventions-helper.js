57b98763b5a05653e1f89a23812efea499deffed
Matt Breckon <matt@mattbreckon.com>
2018-12-05 14:16:27 -0500

    lint

--- tests/dummy/app/components/docs/naming-conventions-helper.js
+++ tests/dummy/app/components/docs/naming-conventions-helper.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 import layout from '../../templates/components/docs/naming-conventions-helper';
 
 export default Component.extend({
-  layout
+  layout,
 });
